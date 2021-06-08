652b175eddb0ca5c4b7035e298db9f0c751df5d4
unknown <jinbo2021@gmail.com>
2021-06-08 12:50:48 +0900

    test

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            WebApplication1.GoDoWa goDowa = new WebApplication1.GoDoWa();
+            string strTemp = string.Empty;
+            strTemp = goDowa.ReturnReturn(new string[] { "저그와 ", "테란" });
+            System.Threading.Thread.Sleep(3000);
+            ViewData["Message"] = strTemp;
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/GoDoWa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class GoDoWa
+    {
+        public string ReturnReturn(string[] x)
+        {
+            string strTemp = string.Empty;
+            try
+            {
+                strTemp = x[0] + x[1];
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.ToString());
+            }
+            return strTemp;
+        }
+    }
+
+}
